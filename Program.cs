0c88ed873b8eaf84bd999900a71866e2b4d68093
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-12-30 12:42:06 +0530

    welcome

--- Program.cs
+++ Program.cs
@@ -12,8 +12,16 @@ namespace Generics
         {
 
             UC1 uc1 = new UC1();
-            int result = uc1.Findmax(33, 1, 2);
-            Console.WriteLine(result);
+
+            //for UC1
+            //int result = uc1.Findmax(1, 2, 3);
+
+            //for UC1.1
+            //int result = uc1.Findmax(33, 1, 2);
+
+            //for UC1.2
+            int result = uc1.Findmax(1,33, 2);
+            Console.WriteLine("Maximum number is : " + result);
             Console.ReadLine();
         }
     }
