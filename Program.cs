0ce8a29932a930af8ae7c0cbecc755e3af5796f5
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-12-30 12:25:09 +0530

    welcome

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            UC1 uc1 = new UC1();
+            int result = uc1.Findmax(1, 2, 3);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
