b2d49ae395c902080736bba70bab8f2e893f997f
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-12-30 12:27:49 +0530

    welcome

---
+++ UC1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class UC1
+    {
+        public T Findmax<T>(T x,T y,T z) where T : IComparable
+        {
+            if (x.CompareTo(y) > 0 && x.CompareTo(z) > 0)
+            {
+                return x;
+
+            }
+            if (y.CompareTo(x) > 0 && y.CompareTo(z) > 0)
+            {
+                return y;
+            }
+            else
+            {
+                return z;
+            }
+        }
+    }
+}
