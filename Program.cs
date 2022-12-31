94060e2f4d7782ab62e2674243b32a068dbd352b
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-12-31 08:14:37 +0530

    welcome

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -13,23 +14,25 @@ namespace Generics
 
         {
 
-            UC1 uc1 = new UC1();
+            //UC1 uc1 = new UC1();
 
             //for UC1
             //int result = uc1.Findmax(1, 2, 3);
             //Console.WriteLine("Maximum number is : " + result);
 
-            //for UC1.1
+            //for UC1.1 find max at 1st postion
             //int result = uc1.Findmax(33, 1, 2);
             //Console.WriteLine("Maximum number is : " + result);
 
-            //for UC1.2
+            //for UC1.2 find max at 2nd postion
             //int result = uc1.Findmax(1,33, 2);
             //Console.WriteLine("Maximum number is : " + result);
 
-            //FOR UC1.3
+            //FOR UC1.3 find max at 3rd postion
             //int result = uc1.Findmax(1, 2, 33);
             //Console.WriteLine("Maximum number is : " + result);
+
+
 
             // UC2
             //float result = uc1.Findmax(1f, 2f, 33f);
@@ -44,9 +47,19 @@ namespace Generics
             //Console.WriteLine("Maximum number is : " + result);
 
             //UC2.3 find max at 3rd position
-            float result = uc1.Findmax(1f, 2f, 33f);
-            Console.WriteLine("Maximum number is : " + result);
+            //float result = uc1.Findmax(1f, 2f, 33f);
+            //Console.WriteLine("Maximum number is : " + result);
+
+
+            //string result = uc1.Findmax("Aaa", "ccc", "xxx");
+            //Console.WriteLine("Maximum string is : " + result);
+            //Console.ReadLine();
+
+            Generics<int> generics = new Generics<int>(30, 30, 50);
+            int result = generics.Genmethod(20, 50, 30);
+            Console.WriteLine("Maximum string is : " + result);
             Console.ReadLine();
+
         }
     }
 }
